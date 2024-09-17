43ff335b7972d33c7f6896310b87f5f46c24b41a
BatanAndrei <yabatan777@gmail.com>
2024-09-17 09:04:07 +0300

    init: init projecr and create screens (ScreenMain, ScreenAboutGame)

---
+++ client_hospice_game/src/App.jsx
@@ -0,0 +1,15 @@
+import ScreenMain from '../src/components/ScreenMain/ScreenMain';
+import ScreenAboutGame from '../src/components/ScreenAboutGame/ScreenAboutGame';
+import ScreenAboutRealCenter from '../src/components/ScreenAboutRealCenter/ScreenAboutRealCenter';
+
+function App() {
+	return (
+		<div>
+			<ScreenMain/>
+			<ScreenAboutGame/>
+			<ScreenAboutRealCenter/>
+		</div>
+	);
+}
+
+export default App;
---
+++ client_hospice_game/src/components/ScreenAboutRealCenter/ScreenAboutRealCenter.jsx
@@ -0,0 +1,12 @@
+import styles from './screenAboutRealCenter.module.css';
+
+
+const ScreenAboutRealCenter = () => {
+    return (
+        <section className={styles.container}>
+            <div className={styles.bigCircle}></div>
+        </section>
+    )
+};
+
+export default ScreenAboutRealCenter;
