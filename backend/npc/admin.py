241c4cd8142ddd4fd61e0a09c44f9bec70aaf813
Hashtagich <antonchikz@mail.ru>
2024-09-30 23:12:03 +0300

    Модели. Добавил модель процедур (начальный уровень).

--- backend/npc/admin.py
+++ backend/npc/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Diagnosis, Patient, Categories, Procedure
+
 
 # Register your models here.
+
+@admin.register(Diagnosis)
+class DiagnosisAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'age', 'condition', 'sentiment', 'fatigue')
+
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+@admin.register(Procedure)
+class ProcedureAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'categories', 'effectiveness')
--- backend/npc/models.py
+++ backend/npc/models.py
@@ -1,3 +1,110 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Patient(models.Model):
+    """Модель пациента"""
+    name = models.CharField(
+        verbose_name='Имя пациента',
+        max_length=15,
+        null=True
+    )
+
+    age = models.PositiveIntegerField(
+        verbose_name='Возраст',
+        default=5
+    )
+
+    condition = models.PositiveIntegerField(
+        verbose_name='Состояние',
+        default=1
+    )
+    sentiment = models.PositiveIntegerField(
+        verbose_name='Настроение',
+        default=1
+    )
+
+    fatigue = models.PositiveIntegerField(
+        verbose_name='Усталость',
+        default=1
+    )
+
+    diagnosis = models.ForeignKey(
+        'Diagnosis',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name='Диагноз'
+    )
+
+    class Meta:
+        verbose_name = 'Пациент'
+        verbose_name_plural = 'Пациенты'
+
+
+class Diagnosis(models.Model):
+    """Модель диагноза"""
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=70,
+        null=True
+    )
+
+    symptoms = models.TextField(
+        verbose_name='Симптомы/описание',
+        null=True
+    )
+
+    class Meta:
+        verbose_name = 'Диагноз'
+        verbose_name_plural = 'Диагнозы'
+
+
+class Categories(models.Model):
+    """Модель категории процедур"""
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=30,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = 'Категория процедур'
+        verbose_name_plural = 'Категории процедур'
+
+
+class Procedure(models.Model):
+    """Модель процедуры"""
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=30,
+        null=True
+    )
+
+    categories = models.ForeignKey(
+        'Categories',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name='Категория'
+    )
+
+    description = models.TextField(
+        verbose_name='Описание',
+        null=True
+    )
+
+    effectiveness = models.TextField(
+        verbose_name='Эффективность',
+        null=True
+    )
+
+    execution_time = models.PositiveIntegerField(
+        verbose_name='Время выполнения',
+        null=True
+    )
+
+    class Meta:
+        verbose_name = 'Процедур'
+        verbose_name_plural = 'Процедуры'
