e8273e704033120c9b4b4f43e2ad018b25692ea1
BatanAndrei <yabatan777@gmail.com>
2024-10-23 11:27:03 +0300

    fix: fix position wrapper video, fix position in footer, delete video file from project, change link in button (to figma)

--- client_hospice_game/src/components/ScreenMain/ScreenMain.jsx
+++ client_hospice_game/src/components/ScreenMain/ScreenMain.jsx
@@ -1,5 +1,6 @@
 import styles from './screenMain.module.css';
 import Button from '../Button/Button';
+//import video from '../../video/videoCastleGame2.0.mp4';
 
 
 const ScreenMain = () => {
@@ -11,10 +12,10 @@ const ScreenMain = () => {
                         <h1 className={styles.title}>Построй, лечи, меняй<br/> жизни детей</h1>
                         <h2 className={styles.subTitle}>Представляем Замок Заботы! Симулятор детского реабилитационного центра!</h2>
                     </div>
-                    <a className={styles.bottomLine} rel="noopener noreferrer" href='https://batanandrei.github.io/bird-build-unity/' target="_blank"><Button className={styles.button} nameButton='Играть'/></a>    
+                    <a className={styles.bottomLine} rel="noopener noreferrer" href='https://www.figma.com/design/wn8i34WXIVj03MoE4tyyBZ/%D0%97%D0%B0%D0%BC%D0%BE%D0%BA-%D0%B7%D0%B0%D0%B1%D0%BE%D1%82%D1%8B?node-id=0-1&node-type=canvas&t=YCW9gKxGRckBfqb3-0' target="_blank"><Button className={styles.button} nameButton='Играть'/></a>    
                 </div>
                 <div className={styles.wrapperVideo}>
-                    <video poster="../../svg/backVideo" muted className={styles.videoPlayer} src="https://videos.pexels.com/video-files/1481903/1481903-sd_640_360_25fps.mp4" controls>
+                    <video poster="../../svg/backVideo" muted className={styles.videoPlayer} src='' controls>
                     </video>
                 </div>
                 <Button className={styles.buttonDisable} nameButton='Играть'/>
